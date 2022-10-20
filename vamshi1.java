fb2d5cd504288df9620a7ed789896da045b3fb66
vamshiveritas <115703370+vamshiveritas@users.noreply.github.com>
2022-10-20 17:50:24 +0530

    Update vamshi1.java

--- vamshi1.java
+++ vamshi1.java
@@ -2,3 +2,6 @@ echo "master branch code...."
 echo "Update in master branch so stop here to do "
 echo "vamshi1 file is started executed by vamshi ......."
 echo "123451234512345123451234512345123451234512345123451234512345........."
+echo " checking the PR req....."
+echo " Peer - peer review......"
+  
