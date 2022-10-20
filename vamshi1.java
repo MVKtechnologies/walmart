58c35f4b25dea220a8e80de8ce1b9db0ef73c97c
vamshiveritas <vamshi.veritas@gmail.com>
2022-10-20 17:18:19 +0530

    update

--- vamshi1.java
+++ vamshi1.java
@@ -1,3 +1,3 @@
-echo "iN vamshi master branch."
+echo "Update in master branch so stop here to do "
 echo "vamshi1 file is started executed by vamshi ......."
 echo "123451234512345123451234512345123451234512345123451234512345........."
